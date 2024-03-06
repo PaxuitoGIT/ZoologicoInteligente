04af68391e6049597e883eecbf57d04c0679fb3d
PaxuitoGIT <bxu@myuax.com>
2024-03-06 11:32:14 +0100

    Clase habitat, getters y setters

---
+++ src/main/java/Zoo/Habitat.java
@@ -0,0 +1,31 @@
+package Zoo;
+
+public class Habitat {
+    private double temperatura;
+    private double humedad;
+    private boolean limpieza;
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public double getHumedad() {
+        return humedad;
+    }
+
+    public void setHumedad(double humedad) {
+        this.humedad = humedad;
+    }
+
+    public boolean isLimpieza() {
+        return limpieza;
+    }
+
+    public void setLimpieza(boolean limpieza) {
+        this.limpieza = limpieza;
+    }
+}
