fea8377e3dd4151d2120c022ea81ee7a33a61820
PaxuitoGIT <bxu@myuax.com>
2024-03-06 11:58:36 +0100

    Habitat aviario

---
+++ src/main/java/Zoo/GestionHabitat/Aviario.java
@@ -0,0 +1,30 @@
+package Zoo.GestionHabitat;
+
+public class Aviario extends Habitat {
+
+    private boolean puedenVolar;
+    private double altura;
+
+    public Aviario(int temperatura, int humedad, boolean limpieza, boolean puedenVolar, double altura) {
+        super(temperatura, humedad, limpieza);
+        this.puedenVolar = puedenVolar;
+    }
+
+    public boolean isPuedenVolar() {
+        return puedenVolar;
+    }
+
+    public void setPuedenVolar(boolean puedenVolar) {
+        this.puedenVolar = puedenVolar;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void ajustarAltura(double altura) {
+        this.altura = altura;
+    }
+
+
+}
