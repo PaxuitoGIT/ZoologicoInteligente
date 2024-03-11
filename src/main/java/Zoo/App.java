1def5a7715e66f2ee70e0d08dbb9ca3e5dd254a8
PaxuitoGIT <bxu@myuax.com>
2024-03-11 19:00:32 +0100

    Menu interacción animales

--- src/main/java/Zoo/App.java
+++ src/main/java/Zoo/App.java
@@ -1,6 +1,7 @@
 package Zoo;
 
 import Zoo.GestionHabitat.*;
+import Zoo.Animales.*;
 
 import java.util.Scanner;
 
@@ -9,6 +10,10 @@ public class App {
         Acuatico acuatico = new Acuatico(25, 80, true, true, 10.0);
         Terrestre terrestre = new Terrestre(30, 60, true, true);
         Aviario aviario = new Aviario(20, 70, true, true, 10.0);
+
+        AnimalTerrestres animalTerrestres = new AnimalTerrestres("Mamífero", "Tigre", "Carne", "Terrestre", "Agresivo", "Saludable", "1", true, true, true);
+        AnimalAcuatico animalAcuatico = new AnimalAcuatico("Pez", "Dorado", "Algas", "Acuatico", "Tranquilo", "Saludable", "2", true, true, true, "Dulce");
+        AnimalAviario animalAviario = new AnimalAviario("Ave", "Canario", "Semillas", "Aviario", "Tranquilo", "Saludable", "3", true, true, true, "Canario");
 
         Monitoreo monitoreo = new Monitoreo();
 
@@ -24,7 +29,8 @@ public class App {
 
         while (true) {
             System.out.println("1. Analizar hábitats");
-            System.out.println("2. Salir");
+            System.out.println("2. Interactuar con animales");
+            System.out.println("3. Salir");
             System.out.print("Seleccione una opción: ");
 
             int opcion = scanner.nextInt();
@@ -34,6 +40,44 @@ public class App {
                     monitoreo.analizarHabitats();
                     break;
                 case 2:
+                    System.out.println("1. Interactuar con animales terrestres");
+                    System.out.println("2. Interactuar con animales acuáticos");
+                    System.out.println("3. Interactuar con animales de aviario");
+                    System.out.print("Seleccione una opción: ");
+
+                    int opcionAnimales = scanner.nextInt();
+
+                    switch (opcionAnimales) {
+                        case 1:
+                            System.out.println("Has seleccionado el animal terrestre: " + animalTerrestres.getNombre());
+                            System.out.println("Especie: " + animalTerrestres.getEspecie());
+                            System.out.println("Alimentación: " + animalTerrestres.getAlimentacion());
+                            System.out.println("Hábitat: " + animalTerrestres.getHabitat());
+                            System.out.println("Comportamiento: " + animalTerrestres.getComportamiento());
+                            System.out.println("Salud: " + animalTerrestres.getSalud());
+                            break;
+                        case 2:
+                            System.out.println("Has seleccionado el animal acuático: " + animalAcuatico.getNombre());
+                            System.out.println("Especie: " + animalAcuatico.getEspecie());
+                            System.out.println("Alimentación: " + animalAcuatico.getAlimentacion());
+                            System.out.println("Hábitat: " + animalAcuatico.getHabitat());
+                            System.out.println("Comportamiento: " + animalAcuatico.getComportamiento());
+                            System.out.println("Salud: " + animalAcuatico.getSalud());
+                            break;
+                        case 3:
+                            System.out.println("Has seleccionado el animal de aviario: " + animalAviario.getNombre());
+                            System.out.println("Especie: " + animalAviario.getEspecie());
+                            System.out.println("Alimentación: " + animalAviario.getAlimentacion());
+                            System.out.println("Hábitat: " + animalAviario.getHabitat());
+                            System.out.println("Comportamiento: " + animalAviario.getComportamiento());
+                            System.out.println("Salud: " + animalAviario.getSalud());
+                            break;
+                        default:
+                            System.out.println("Opción no válida. Por favor, intente de nuevo.");
+                            break;
+                    }
+                    break;
+                case 3:
                     System.out.println("Saliendo...");
                     return;
                 default:
