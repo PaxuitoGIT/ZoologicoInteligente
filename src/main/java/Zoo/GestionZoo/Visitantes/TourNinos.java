223d4ef3bf7d196b0d342f99c41e0010881fb6df
PaxuitoGIT <bxu@myuax.com>
2024-03-13 21:46:15 +0100

    Corrección texto

--- src/main/java/Zoo/GestionZoo/Visitantes/TourNinos.java
+++ src/main/java/Zoo/GestionZoo/Visitantes/TourNinos.java
@@ -4,9 +4,9 @@ public class TourNinos implements Tour {
     @Override
     public void realizarTour() {
         System.out.println("Bienvenidos al tour para niños!");
-        System.out.println("Primero, vamos a visitar el aviario, donde podrán ver diferentes tipos de aves.");
-        System.out.println("Luego, iremos al área de los animales terrestres, donde podrán ver tigres.");
-        System.out.println("Después, iremos al área de los animales acuáticos, donde podrán ver peces.");
+        System.out.println("Primero, vamos a visitar el aviario, donde podrán ver diferentes desde loros hasta águilas.");
+        System.out.println("Luego, iremos al área de los animales terrestres, donde podrán ver desde koalas hasta tigres.");
+        System.out.println("Después, iremos al área de los animales acuáticos, donde podrán ver desde nutrias hasta tiburones .");
         System.out.println("Finalmente, tendremos una actividad de alimentación de animales. Recuerden, siempre debemos respetar a los animales y seguir las instrucciones del guía.");
         System.out.println("¡Esperamos que disfruten el tour!");
         System.out.println();
