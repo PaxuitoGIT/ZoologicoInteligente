74dedc155f529a75d10110e2d8eccf3c73200c16
PaxuitoGIT <bxu@myuax.com>
2024-03-13 16:30:57 +0100

    Recurso

---
+++ src/main/java/Zoo/GestionZoo/Administracion/Recurso.java
@@ -0,0 +1,23 @@
+package Zoo.GestionZoo.Administracion;
+
+public class Recurso {
+    private String nombre;
+    private int cantidad;
+
+    public Recurso(String nombre, int cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
